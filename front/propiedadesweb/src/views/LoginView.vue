bd99e87ef7aa8a246038ed2d4bd9963fcdccb1ab
reii23 <reinaldo.pacheco@usach.cl>
2024-08-17 15:39:47 -0400

    update register and login
    se actualiza el registro de un usuario al agregar la opción de escoger el rol de usuario que quiere registrar (0: admin, 1: usuario, 2: agente inmobiliario) y en el login se agrega manejo de errores para credenciales inválidas.

--- front/propiedadesweb/src/views/LoginView.vue
+++ front/propiedadesweb/src/views/LoginView.vue
@@ -10,36 +10,63 @@
         <label for="password">Contraseña</label>
         <input type="password" id="password" v-model="user.password" required>
       </div>
-      <button type="submit">Iniciar Sesión</button>
+      <button type="submit":disabled="isSubmitting">
+        {{ isSubmitting ? 'Iniciando sesión...' : 'Iniciar Sesión' }}
+      </button>
     </form>
+    <p v-if="message":class="{ 'success-message': isSuccess, 'error-message': !isSuccess }">
+      {{ message }}
+    </p>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 import {auth} from "@/auth";
 
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     return {
       user: {
-        email: '',
-        password: ''
-      }
+        email: "",
+        password: ""
+      },
+      isSubmitting: false,
+      message: "",
+      isSuccess: false
     }
   },
+
+
   methods: {
     async loginUser() {
+      this.isSubmitting = true;
+      this.message = "";
       try {
-        const response = await axios.post('http://localhost:8080/user/login', this.user);
-        // Obtengo el usuario a traves del id para obtener el rol y el id
-        const response2 = await axios.get(`http://localhost:8080/user/obtenerUsuarioPorEmail/${this.user.email}`);
-        console.log(response2.data.id);
-        auth.login(response2.data.id, response2.data.rol); // Actualiza el estado reactivo
-        this.$router.push('/');
-      } catch (error) {
-        console.error('Error al iniciar sesión:', error);
+        const loginResponse = await axios.post("http://localhost:8080/user/login", this.user);
+        
+        if (loginResponse.data && loginResponse.data.success) {
+          const userResponse = await axios.get(`http://localhost:8080/user/obtenerUsuarioPorEmail/${this.user.email}`);
+          if (userResponse.data) {
+            auth.login(userResponse.data.id, userResponse.data.rol);
+            this.message = "Has iniciado sesión correctamente";
+            this.isSuccess = true;
+            setTimeout(() => this.$router.push("/"), 1500);
+          } else {
+            throw new Error("No se pudo obtener la información del usuario");
+          }
+
+        } else{
+          throw new Error("Credenciales ingresadas inválidas");
+        }
+        
+      } catch (error){
+        console.error("Error al iniciar sesion:", error);
+        this.message = error.response?.data?.message || "Error al iniciar sesión. Inténtalo de nuevo";
+        this.isSuccess = false;
+      } finally{
+        this.isSubmitting = false;
       }
     }
   }
@@ -51,21 +78,27 @@ export default {
   max-width: 350px;
   margin: 0 auto;
   padding: 20px;
+  border-radius: 8px;
 }
 .form-group {
   margin-bottom: 15px;
 }
+
 label {
   display: block;
   margin-bottom: 5px;
+  font-weight: bold;
 }
+
 input {
   width: 100%;
   padding: 8px;
   border: 1px solid #ddd;
   border-radius: 4px;
+  font-size: 16px;
 }
-button {
+
+button{
   width: 100%;
   padding: 10px;
   background-color: rgb(255,146,2);
@@ -73,8 +106,22 @@ button {
   border: none;
   border-radius: 4px;
   cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s ease;
+
 }
+
 button:hover {
   background-color: rgb(8, 8, 8);
 }
+
+.success-message{
+  color: green;
+  font-weight: bold;
+}
+.error-message{
+  color: red;
+  font-weight: bold;
+}
+
 </style>
--- front/propiedadesweb/src/views/RegisterView.vue
+++ front/propiedadesweb/src/views/RegisterView.vue
@@ -1,14 +1,26 @@
 <template>
     <div class="register-container">
-      <h2>Registro de Usuario</h2>
+      <h2 class="titulo-container">Registro de Usuario</h2>
       <form @submit.prevent="registerUser">
         <div class="form-group">
           <label for="nombre">Nombre de usuario</label>
           <input type="text" id="nombre" v-model="user.nombre" required>
         </div>
         <div class="form-group">
-          <label for="email">Emai:</label>
+          <label for="email">Email</label>
           <input type="email" id="email" v-model="user.email" required>
+        </div>
+        <div class = "form-group">
+          <label for="numeroTelefono">Numero de Telefono</label>
+          <input type="text" id="numeroTelefono" v-model="user.numeroTelefono" required>
+        </div>
+        <div class="form-group">
+          <label for="rol">Rol</label>
+          <select id="rol" v-model="user.rol">
+            <option value="0">Administrador</option>
+            <option value="1">Usuario</option>
+            <option value="2">Agente Inmobiliario</option>
+          </select>
         </div>
         <div class="form-group">
           <label for="password">Contraseña</label>
@@ -32,25 +44,30 @@
         user: {
           nombre: '',
           email: '',
-          password: ''
+          password: '',
+          numeroTelefono: '',
+          rol: 1 // por defecto el usuario comprador o arrendador (usuario normal)
         },
         message: '',
-        isSuccess: false
+        isSuccess: false,
+        isSubmitting: false
       }
     },
     methods: {
       async registerUser() {
+        this.isSubmitting = true;
         try {
           const response = await axios.post('http://localhost:8080/user/crearUsuario', this.user);
           console.log('Usuario registrado:', response.data);
           this.message = 'Usuario registrado exitosamente';
           this.isSuccess = true;
-          // Aquí puedes redirigir al usuario después de un breve retraso
           setTimeout(() => this.$router.push('/login'), 2000);
         } catch (error) {
           console.error('Error al registrar usuario:', error);
           this.message = 'Error al registrar usuario. Intentalo de nuevo';
           this.isSuccess = false;
+        } finally {
+          this.isSubmitting = false;
         }
       }
     }
@@ -62,19 +79,28 @@
     max-width: 350px;
     margin: 0 auto;
     padding: 20px;
+    border-radius: 8px;
+  }
+
+  .titulo-container {
+    text-align: center;
+    color: rgb(255, 255, 255);
   }
   .form-group {
     margin-bottom: 15px;
+    color:rgb(255, 250, 250);
   }
   label {
     display: block;
     margin-bottom: 5px;
+    font-weight: bold;
   }
-  input {
+  input, select {
     width: 100%;
     padding: 8px;
-    border: 1px solid #ddd;
+    border: 1px solid #070707;
     border-radius: 4px;
+    font-size: 16px;
   }
   button {
     width: 100%;
@@ -84,8 +110,23 @@
     border: none;
     border-radius: 4px;
     cursor: pointer;
+    font-size: 16px;
   }
   button:hover {
     background-color: rgb(8, 8, 8);
   }
+
+  button:disabled {
+  background-color: #cccccc;
+  cursor: not-allowed;
+}
+
+.succes-message {
+  color: green;
+}
+
+.error-message {
+  color: red;
+}
+
   </style>
